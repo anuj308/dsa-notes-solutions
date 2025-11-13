095edd2a6b17d092c452f881f096d2fb13dd2092
anuj308 <anujkumarsharma2023@gmail.com>
2025-11-13 19:56:52 +0000

    a dp problem from tuf

--- tuf/32DPPart1.cpp
+++ tuf/32DPPart1.cpp
@@ -53,3 +53,83 @@
 // 1) try to represent the problem in terms of index
 // 2) do all possiable stuffs on that end in according to problem
 // 3) sum of all stuffs -> count all ways, min (of all the suffs) ->find min
+
+// 1D dp
+// recursion to dp - memorization(top down) - store sub problems - tabulation(bottom up)
+
+// memorization(top down)
+class Solution {
+  public:
+    int f(vector<int>& height,int i){
+        if(i==0) return 0;
+        
+        int left = f(height,i-1) + abs(height[i]-height[i-1]);
+        int right = INT_MAX;
+        if(i>1) right = f(height,i-2) + abs(height[i]-height[i-2]);
+        return min(left,right);
+    }
+    int minCost(vector<int>& height) {
+        int n = height.size();
+        
+        return f(height,n-1);
+    }
+};
+
+//  store sub problems 
+class Solution {
+  public:
+    int f(vector<int>& height,int i,vector<int>& dp){
+        if(i==0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int left = f(height,i-1,dp) + abs(height[i]-height[i-1]);
+        int right = INT_MAX;
+        if(i>1) right = f(height,i-2,dp) + abs(height[i]-height[i-2]);
+        return dp[i] = min(left,right);
+    }
+    int minCost(vector<int>& height) {
+        // Code here
+        int n = height.size();
+        vector<int> dp(n,-1);
+        return f(height,n-1,dp);
+    }
+};
+
+// tabulation(bottom up)
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+        // Code here
+        int n = height.size();
+        vector<int> dp(n,-1);
+        dp[0]=0;
+        for(int i=1;i<n;i++){
+            int one = INT_MAX;
+            int two = INT_MAX;
+            if(i>=1) one = dp[i-1] + abs(height[i]-height[i-1]);
+            if(i>=2) two = dp[i-2] + abs(height[i]-height[i-2]);
+            dp[i]=min(one,two);
+        }
+        return dp[n-1];
+    }
+};
+
+// space optimization 
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+        // Code here
+        int n = height.size();
+        int prev = 0;
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int one = INT_MAX;
+            int two = INT_MAX;
+            if(i>=1) one = prev + abs(height[i]-height[i-1]);
+            if(i>=2) two = prev2 + abs(height[i]-height[i-2]);
+            int cur =min(one,two);
+            prev2 = prev;
+            prev = cur;
+        }
+        return prev;
+    }
+};
