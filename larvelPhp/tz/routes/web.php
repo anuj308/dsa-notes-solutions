01be7ece6a6913d1067211d36f39effa49be2e41
anuj308 <anujkumarsharma2023@gmail.com>
2026-04-01 15:14:51 +0530

    php larvel class breeze

--- larvelPhp/tz/routes/web.php
+++ larvelPhp/tz/routes/web.php
@@ -9,3 +9,18 @@ Route::get('/', function () {
 Route::get('/monday', function () {
     return view('monday');
 });
+
+Route::get('/header', function () {
+    return response("Custom Header Example")->header('X-App-Name'.'MyLarvelApp');
+});
+
+Route::get('/header', function () {
+    return response("Custom Header Example")->header('X-App-Name'.'MyLarvelApp')
+    ->header('X-version'.'1.0');
+});
+
+Route::domain('hello.com')->group(function(){
+    Route::get("/",function(){
+        return response("Welcome user");
+    });
+});
